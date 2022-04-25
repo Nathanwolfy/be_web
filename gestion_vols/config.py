64ffc9f0fe11e1f55fd7838445b16759948ea0d1
Nathanwolfy <nathan.ldgb@gmail.com>
2022-04-25 12:07:24 +0200

    auth adjustments

--- gestion_vols/config.py
+++ gestion_vols/config.py
@@ -3,4 +3,15 @@ DEBUG = True
 WEB_SERVER = {"host": "localhost",
             "port":8080,} #Configuration du serveur de BDD
 
+#Configuration du serveur de BDD
+DB_SERVER = {
+"user": "root",
+"password": "mysql",
+"host": "localhost",
+"port": 3306, #8889 si MAC
+"database": "gsea_cours", #nom de la BDD
+"raise_on_warnings": True
+}
+
+
 SECRET_KEY="79a6a933dfc9b1975e444d4e8481c64c771d8ab40b7ac72f8bc1a1bca1718bef"
--- gestion_vols/controller/bdd.py
+++ gestion_vols/controller/bdd.py
@@ -1,21 +1,24 @@
-"""import mysql
+import mysql.connector
+from mysql.connector import errorcode
+from ..config import DB_SERVER
 
-def verifAuthData(login, mdp):
+# connexion au serveur de BDD
+def connexion():
+    cnx = ""
     try:
-        cnx, error = connexion()
-        if error is not None:
-            return error, None
-        cursor = cnx.cursor(dictionary=True)
-        sql = "SELECT * FROM identification WHERE login=%s and motPasse=%s"
-        param=(login, mdp)
-        cursor.execute(sql, param)
-        user = cursor.fetchone()
-        close_bd(cursor, cnx)
-        msg = "authOK"
+        cnx = mysql.connector.connect(**DB_SERVER)
+        error = None
     except mysql.connector.Error as err:
-        user = None
-        msg = "Failed get Auth data : {}".format(err)
-        return msg, user"""
+        error = err
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Mauvais login ou mot de passe")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("La Base de données n'existe pas.")
+        else:
+            print(err)
+    return cnx, error # error: remonte problème connexion
 
-def verifAuthData(login,mdp):
-    return "authOK",[] 
+# fermeture de la connexion au serveur de BDD
+def close_bd(cursor, cnx):
+    cursor.close()
+    cnx.close()
